c685f7607c6d9545c1b0fc119990369ed3d00b95
richardp23 <richard.p23@icloud.com>
2023-04-28 16:54:45 -0400

    1 file modified

--- primitives.js
+++ primitives.js
@@ -2,15 +2,15 @@ import { render } from './util';
 
 // TODO: Declare a variable, assign it a number, and return it.
 render('primitives1', () => {
-
+  let variable = 0;
+  return variable;
 });
 
 // TODO: Declare a variable, assign it a boolean, and return it.
 render('primitives2', () => {
-  
+  let variable = true;
+  return variable;
 });
 
 // TODO: Declare a variable, assign it to null, and return it.
-render('primitives3', () => {
-  
-});
+render('primitives3', () => {});
